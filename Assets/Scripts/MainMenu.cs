518e0e308618987cd740bd0e56c098053d2c328d
Robertas <meska.lt@gmail.com>
2016-01-17 16:36:30 +0200

    Implemented very basic main menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// Main menu.
+/// Attached to Main Camera
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public Texture backgroundTexture;
+
+	void OnGUI() {
+		// Display our background sprite
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+		// Display buttons
+		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "Start")) {
+			Debug.Log("Start button clicked.");
+			Application.LoadLevel("Main");
+		}
+	}
+}
